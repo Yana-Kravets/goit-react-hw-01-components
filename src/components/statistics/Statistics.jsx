3b5a57a7161c54e55cf8d726c3a3d49114188d9f
Sanderrica <99506840+Sanderrica@users.noreply.github.com>
2023-05-21 15:27:21 +0200

    add task

--- src/components/statistics/Statistics.jsx
+++ src/components/statistics/Statistics.jsx
@@ -1,5 +1,5 @@
 import PropTypes from 'prop-types';
-// import css from './Statistics.module.css';
+import css from './Statistics.module.css';
 
 function getRandomHexColor() {
   let bgColor = `#${Math.floor(Math.random() * 16777215).toString(16)}`;
@@ -7,18 +7,18 @@ function getRandomHexColor() {
 }
 
 export const Statistics = ({ title, stats }) => (
-  <div className="statisticsContainer">
-      <section className="statistics">
-        {title && <h2 className="title">{title}</h2>}
-        <ul className="stat-list">
+    <div className={css.statisticsContainer}>
+      <section className={css.statistics}>
+            {title && <h2 className={css.title}>{title}</h2>}
+        <ul className={css.stat-list}>
             {stats.map(stat => (
                 <li key={stat.id}
-                    className="item"
+                    className={css.item}
                     style={{
                         backgroundColor: getRandomHexColor()
                     }}>
-                    <span className="label">{stat.label}</span>
-                    <span className="percentage">{stat.percentage}</span>
+                    <span className={css.label}>{stat.label}</span>
+                    <span className={css.percentage}>{stat.percentage}</span>
                 </li>
             ))}   
         </ul>
